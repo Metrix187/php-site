1403d707b9423e8474131c58ebac203876530cb1
Metrix187 <33392918+Metrix187@users.noreply.github.com>
2019-01-11 17:11:53 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+
+ <!DOCTYPE html>
+<html>
+<head>
+ <meta charset = "Utf-8">
+ <title>PHP-Bison</title>
+<body>
+
+<----------This is a test php script---------->
+
+<h1>Test Big Text</h1>
+<p>Test Small Text</p>
+
+</body>
+</html> 
